c63be4669cee86e3ae6ab61b1c01a1fd5aa2467c
Mike Boby <bestillasbright@gmail.com>
2013-12-03 02:12:28 -0500

    Change internal sigs to lnc for brevity

--- lncurses.c
+++ lncurses.c
@@ -8,7 +8,7 @@
 static const char *LNC_WINDOW_MT = "lnc.window";
 static const char *LNC_STDSCR_MT = "lnc.stdscr";
 
-// lncurses.init()
+// lnc.init()
 static int lnc_init(lua_State *L)
 {
 	WINDOW **l_stdscr = lua_newuserdata(L, sizeof(*l_stdscr));
@@ -18,14 +18,14 @@ static int lnc_init(lua_State *L)
 	return 1;
 }
 
-// lncurses.quit()
+// lnc.quit()
 static int lnc_quit(lua_State *L)
 {
 	endwin();
 	return 0;
 }
 
-// lncurses.stdscr() -> lncurses.Window
+// lnc.stdscr() -> lnc.Window
 static int lnc_stdscr(lua_State *L)
 {
 	WINDOW **l_stdscr = lua_newuserdata(L, sizeof(*l_stdscr));
@@ -34,7 +34,7 @@ static int lnc_stdscr(lua_State *L)
 	return 1;
 }
 
-// lncurses.nl(on: boolean)
+// lnc.nl(on: boolean)
 static int lnc_nl(lua_State *L)
 {
 	luaL_checktype(L, -1, LUA_TBOOLEAN);
@@ -47,7 +47,7 @@ static int lnc_nl(lua_State *L)
 	return 0;
 }
 
-// lncurses.cbreak(on: boolean)
+// lnc.cbreak(on: boolean)
 static int lnc_cbreak(lua_State *L)
 {
 	luaL_checktype(L, -1, LUA_TBOOLEAN);
@@ -60,7 +60,7 @@ static int lnc_cbreak(lua_State *L)
 	return 0;
 }
 
-// lncurses.echo(on: boolean)
+// lnc.echo(on: boolean)
 static int lnc_echo(lua_State *L)
 {
 	luaL_checktype(L, -1, LUA_TBOOLEAN);
@@ -73,7 +73,7 @@ static int lnc_echo(lua_State *L)
 	return 0;
 }
 
-// lncurses.raw(on: boolean)
+// lnc.raw(on: boolean)
 static int lnc_raw(lua_State *L)
 {
 	luaL_checktype(L, -1, LUA_TBOOLEAN);
@@ -86,7 +86,7 @@ static int lnc_raw(lua_State *L)
 	return 0;
 }
 
-// lncurses.halfdelay(tenths: int)
+// lnc.halfdelay(tenths: int)
 static int lnc_halfdelay(lua_State *L)
 {
 	int tenths = luaL_checkint(L, -1);
@@ -96,7 +96,7 @@ static int lnc_halfdelay(lua_State *L)
 	return 0;
 }
 
-// lncurses.qiflush(on: boolean)
+// lnc.qiflush(on: boolean)
 static int lnc_qiflush(lua_State *L)
 {
 	luaL_checktype(L, -1, LUA_TBOOLEAN);
@@ -109,14 +109,14 @@ static int lnc_qiflush(lua_State *L)
 	return 0;
 }
 
-// lncurses.lines() -> int
+// lnc.lines() -> int
 static int lnc_lines(lua_State *L)
 {
 	lua_pushinteger(L, LINES);
 	return 1;
 }
 
-// lncurses.cols() -> int
+// lnc.cols() -> int
 static int lnc_cols(lua_State *L)
 {
 	lua_pushinteger(L, COLS);
